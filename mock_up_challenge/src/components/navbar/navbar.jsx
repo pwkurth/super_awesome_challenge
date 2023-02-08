99a27b0bcd5819ca0cfc042d176f966fedaf749d
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-07 21:14:37 -0600

    Update company logo

--- mock_up_challenge/src/components/navbar/navbar.jsx
+++ mock_up_challenge/src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import avatarIcon from "../../assets/navbar/bussinyaak.png";
-import genericLogo from "../../assets/navbar/generic-logo.png";
+import genericLogo from "../../assets/navbar/black-mesa-logo.svg";
 
 const Navbar = () => {
   const [userName] = useState("JB");
@@ -14,28 +14,28 @@ const Navbar = () => {
           class="flex items-center mr-0 ml-10 "
         >
           <img src={genericLogo} class="h-6  sm:h-9 pr-2" alt="Flowbite Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <span class="self-center text-xl font-semibold whitespace-nowrap ml-2 dark:text-white hover:text-green-500">
             Best Website Ever
           </span>
         </a>
         <a href="/home" class="flex items-center ml-10 mr-10">
-          <span class="self-center text-xl whitespace-nowrap dark:text-white">
+          <span class="self-center text-xl whitespace-nowrap dark:text-white hover:text-green-500 ">
             Home
           </span>
         </a>
         <a href="/about" class="flex items-center ml-0 mr-10">
-          <span class="self-center text-xl whitespace-nowrap dark:text-white">
+          <span class="self-center text-xl whitespace-nowrap dark:text-white hover:text-green-500">
             About
           </span>
         </a>
         <div class="flex items-center ml-auto mr-4">
-          <span class="self-center text-xl whitespace-nowrap dark:text-white">
+          <span class="self-center text-xl whitespace-nowrap dark:text-white ">
             Hello, {userName}
           </span>
         </div>
         <div class="flex items-center ml-0 mr-10 ">
           <img
-            class="w-12 h-12 rounded-full"
+            class="w-12 h-12 rounded-full cursor-pointer "
             src={avatarIcon}
             alt="JB Killing It"
           />
