c1fb205b101de5c5d0253a86e2c766ddb8ded7e0
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-10 23:40:59 -0600

    Add some background color and margin on site

--- mock_up_challenge/src/pages/Home/Home.jsx
+++ mock_up_challenge/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import DocumentCartoon from "../../assets/home/document-cartoon.png";
 
 const Home = () => {
   return (
-    <div>
+    <div class="bg-slate-100 mt-16 mr-16 ml-16">
       <div class="flex flex-row">
         <div class="basis-2/4 p-20">
           <p class=" text-2xl font-poppins">
@@ -17,7 +17,7 @@ const Home = () => {
         </div>
         <div class="basis-1/2 ">
           <img
-            class="md:container md:mx-auto object-contain p-20 "
+            class="md:container md:mx-auto object-contain p-20"
             src={DocumentCartoon}
             alt={"Document Cartoon"}
           />
