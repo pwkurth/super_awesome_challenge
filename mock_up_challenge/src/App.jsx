2609fb644819d60d090a02363a83fb0f44a24bb8
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-10 23:02:55 -0600

    Add home to App.jsx

--- mock_up_challenge/src/App.jsx
+++ mock_up_challenge/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
+import Home from "./pages/Home/Home";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
