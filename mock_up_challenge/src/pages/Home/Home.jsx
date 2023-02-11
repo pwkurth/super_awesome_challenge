5b6f72267bf4a3b12a1a5d9ebf2c1b0f175f227c
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-10 23:27:19 -0600

    Lorem text, image added to home page

--- mock_up_challenge/src/pages/Home/Home.jsx
+++ mock_up_challenge/src/pages/Home/Home.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import DocumentCartoon from "../../assets/home/document-cartoon.png";
 
-const Home = () => {};
+const Home = () => {
+  return (
+    <div>
+      <div class="flex flex-row">
+        <div class="basis-2/4 p-20">
+          <p class=" text-2xl font-poppins">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam
+            nulla porttitor massa id neque aliquam vestibulum morbi. Enim
+            facilisis gravida neque convallis a cras. Quis vel eros donec ac
+            odio tempor orci. Orci a scelerisque purus semper eget duis at
+            tellus at.
+          </p>
+        </div>
+        <div class="basis-1/2 ">
+          <img
+            class="md:container md:mx-auto object-contain p-20 "
+            src={DocumentCartoon}
+            alt={"Document Cartoon"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
