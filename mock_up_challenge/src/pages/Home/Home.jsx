12f7b10714658ced701ab6768e47f2ac11420edd
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-11 00:09:07 -0600

    Change up some css styling on home

--- mock_up_challenge/src/pages/Home/Home.jsx
+++ mock_up_challenge/src/pages/Home/Home.jsx
@@ -5,22 +5,25 @@ const Home = () => {
   return (
     <div class="bg-slate-100 mt-16 mr-16 ml-16 shadow-md">
       <div class="flex flex-row">
-        <div class="basis-2/4 p-20">
-          <p class=" text-2xl font-poppins">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam
-            nulla porttitor massa id neque aliquam vestibulum morbi. Enim
-            facilisis gravida neque convallis a cras. Quis vel eros donec ac
-            odio tempor orci. Orci a scelerisque purus semper eget duis at
-            tellus at.
+        <div class="basis-1/2 p-20">
+          <p class="text-center leading-relaxed text-2xl font-poppins">
+            Did you hear that? They've shut down the main reactor. We'll be
+            destroyed for sure. This is madness! We're doomed! There'll be no
+            escape for the Princess this time. What's that? Artoo! Artoo-Detoo,
+            where are you? At last! Where have you been? They're heading in this
+            direction. Where are those transmissions you intercepted? What have
+            you done with those plans? We intercepted no transmissions.
+            Aaah....This is a consular ship. Were on a diplomatic mission. If
+            this is a consular ship...were is the Ambassador? Commander, tear
+            this ship apart until you've found those plans and bring me the
+            Ambassador. These astro-droids are getting quite out of hand. Even I
+            can't understand their logic at times. How could I be so stupid?
+            He's nowhere in sight. Blast it! Pardon me, sir, but couldn't we go
+            after him? It's too dangerous with all the Sandpeople around.
           </p>
         </div>
-        <div class="basis-1/2 ">
-          <img
-            class="md:container md:mx-auto object-contain p-20"
-            src={DocumentCartoon}
-            alt={"Document Cartoon"}
-          />
+        <div class="container basis-1/2 p-20  items-center justify-center">
+          <img class="mx-auto" src={DocumentCartoon} alt={"Document Cartoon"} />
         </div>
       </div>
     </div>
