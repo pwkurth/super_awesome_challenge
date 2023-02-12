b7841e3f8a30a7eab259464f9e396207068e2df8
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-12 13:12:51 -0600

    Updated styling on cardloader

--- mock_up_challenge/src/components/cards/cardloader.jsx
+++ mock_up_challenge/src/components/cards/cardloader.jsx
@@ -2,30 +2,25 @@ import React from "react";
 
 const CardLoader = (props) => {
   return (
-    <div class="font-poppins max-w-sm p-6 rounded-lg  dark:bg-gray-800 dark:border-gray-700">
-      <div class="border-b-2 border-b-indigo-500">
-        <h5 class="text-2xl  font-bold tracking-tight  text-gray-900 dark:text-white">
+    <div class="relative flex flex-col font-poppins max-w-sm h-[24rem] p-6 rounded-lg  bg-gray-800 border-gray-700">
+      <div class="flex flex-row border-b-2 border-b-indigo-500">
+        <h5 class="text-2xl  font-bold tracking-tight   text-white hover:text-green-300 ">
           {props.firstName} {props.lastName}
         </h5>
+        <div class="inline-flex ml-auto absolute right-2 top-2 items-center px-2 py-2 text-sm font-medium text-center text-white bg-white rounded-3xl "></div>
       </div>
-      <h6 class="mb-2 mt-2 text-xl  tracking-tight text-gray-900 dark:text-white">
+      <h6 class="mb-2 mt-1 text-xl  tracking-tight text-white">
         Favorite Fact
       </h6>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {props.funFact}
-      </p>
-      <a
-        href="#"
-        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Read more
-      </a>
-      <a
-        href="#"
-        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        {props.favNumber}
-      </a>
+      <p class="mb-3 font-normal text-indigo-200">{props.funFact}</p>
+      <div class="mt-auto">
+        <a
+          href="#"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 "
+        >
+          Read more
+        </a>
+      </div>
     </div>
   );
 };
