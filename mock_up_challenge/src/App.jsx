c17bb47f5e388c038565862083b68cb93d0b0bcc
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-14 21:39:55 -0600

    Added landing wip

--- mock_up_challenge/src/App.jsx
+++ mock_up_challenge/src/App.jsx
@@ -3,15 +3,17 @@ import Navbar from "./components/navbar/navbar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import CSVPage from "./pages/CSVPage/CSVPage";
+import Landing from "./pages/Landing/Landing";
 import "./index.css";
 
 function App() {
   return (
     <div className="App bg-[#222831]">
-      <Navbar />
+      <Landing />
+      {/* <Navbar />
       <Home />
       <About />
-      <CSVPage />
+      <CSVPage /> */}
     </div>
   );
 }
