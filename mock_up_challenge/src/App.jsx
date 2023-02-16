2643d845e0666abea22ed44f4c52e67174380f8a
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-02-15 21:20:24 -0600

    UI CHANGES

--- mock_up_challenge/src/App.jsx
+++ mock_up_challenge/src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Modal from "./components/modal/modal";
 import Navbar from "./components/navbar/navbar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -8,15 +11,23 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="App h-[100vh] bg-gray-900">
-      {/* <Navbar /> */}
-      <Landing />
-      <About />
-
-      {/* 
-
-
-      <CSVPage /> */}
+    <div className="App h-[100vh]  bg-gray-900">
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Layout
+                landing={<Landing />}
+                about={<About />}
+                modal={<Modal />}
+              />
+            }
+          />
+          <Route exact path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ mock_up_challenge/src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Landing from "../../pages/Landing/Landing";
+import About from "../../pages/About/About";
+import Modal from "../modal/modal";
+
+const Layout = (props) => {
+  return <>{props.landing}</>;
+};
+
+export default Layout;
